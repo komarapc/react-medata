a452485df3fb07efee9bd15d847666b7059ebc1d
izmi.komar <komar.izmi@gmail.com>
2024-01-02 19:02:52 +0700

    meta content

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,28 @@ function App() {
     <>
       <Helmet>
         <title>React App MetaData</title>
+        <meta name="description" content="Hello There Good Morning" />
+        <meta property="og:url" content="https://react-medata.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Title Content" />
+        <meta property="og:description" content="Hello There" />
+        <meta
+          property="og:image"
+          content="https://opengraph.b-cdn.net/production/documents/45bca4ba-2d02-4f5e-94aa-6f64fcf78926.jpg?token=AMWIPvDviWvP90KyvJ1wG0o5HFVrMcV3_FLg-i23iqo&height=675&width=1200&expires=33240196487"
+        />
 
-        <title>Title Content</title>
-        <meta name="description" content="Hello There" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="react-medata.vercel.app" />
+        <meta
+          property="twitter:url"
+          content="https://react-medata.vercel.app/"
+        />
+        <meta name="twitter:title" content="Title Content" />
+        <meta name="twitter:description" content="Hello There" />
+        <meta
+          name="twitter:image"
+          content="https://opengraph.b-cdn.net/production/documents/45bca4ba-2d02-4f5e-94aa-6f64fcf78926.jpg?token=AMWIPvDviWvP90KyvJ1wG0o5HFVrMcV3_FLg-i23iqo&height=675&width=1200&expires=33240196487"
+        />
       </Helmet>
       <div>
         <a href="https://vitejs.dev" target="_blank">
